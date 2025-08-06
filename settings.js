f5286170f75de8ac2a8f36d247509da8a0b1483b
anurakum0 <anurakum06@gmail.com>
2025-08-07 01:35:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;GBVnHJTQ#GUS2y6kkg1p6_PrFn9gvmg52JIqWVy1IRvon3T7KkQs", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "94784967601", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
