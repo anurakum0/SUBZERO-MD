7d1a971f2683ae4200830fc6562da0254958adb3
anurakum0 <anurakum06@gmail.com>
2025-08-07 01:56:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;GBVnHJTQ#GUS2y6kkg1p6_PrFn9gvmg52JIqWVy1IRvon3T7KkQs", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "94784967601", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;SUBZERO-MD;;;eAUS3agQ#CUKn8Jjww-EcI_sUJMWxerV6JMr7aXTLSJ6zvUHh6hw", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "94756425439", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
